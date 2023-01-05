301a74ab5c8c2a5c6737f49c4c4af9514213fbf6
Glaucobras <121987677+Glaucobras@users.noreply.github.com>
2023-01-05 17:48:51 -0300

    Update Functions in R by Glauco.R

--- Functions in R by Glauco.R
+++ Functions in R by Glauco.R
@@ -1,7 +1,7 @@
 # Script to save some functions created by Glauco G. Silveira
 
 
-
+# 1
 
 # Function to multiply all elements of 2 vectors element by element.
 # It will create a new vector with lenght iqual to lenght(vec1)*lenght(vec2)
@@ -14,11 +14,11 @@
 vecmultiply <- function (vector1,vector2){
   a<-c()
   count<-1
-  count3<-1
+  count2<-1
   for (i in vector1) {
     for (b in vector2){
-      a[count3]<-(b*i)
-      count3<-count3+1
+      a[count2]<-(b*i)
+      count2<-count2+1
     }
     
   }
@@ -28,7 +28,7 @@ vecmultiply <- function (vector1,vector2){
 }
 
 #Example :
-vec1<-c(1,2,3,4,4,5,6,7,8)
+vec1<-c(1,2,3,4,5,6,7,8,9)
 vec2<-c(10,20,30,40)
 
 vecmultiply(vec1,vec2)
