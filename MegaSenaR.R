27d9ea0a3cde3896a5696b259386af33fe546217
Glaucobras <121987677+Glaucobras@users.noreply.github.com>
2023-01-09 23:35:23 -0300

    Update MegaSenaR.R

--- MegaSenaR.R
+++ MegaSenaR.R
@@ -51,7 +51,7 @@ bola4
 bola5
 bola6
 
-
+#Vetor com todos os números sorteados em todos os concursos
 bolaTotal<-c(bola1,bola2,bola3,bola4,bola5,bola6)
 
 
@@ -61,9 +61,10 @@ moda = function(dados) {
   names(vetor)[vetor == max(vetor)]
 }
 
-
+#Valor que mais saiu dentre os 60 números
 moda(bolaTotal)
 
+#Soma das ocorrências de todas as bolas independente da ordem e do sorteio.
 somaBola<-data.frame()
 for (i in 1:60){
   somaBola[i,1]<-i
@@ -72,13 +73,43 @@ for (i in 1:60){
   
 }
 names(somaBola)<- c("Bola","SomaTotal")
+somaBola
 
-porcentagem <- somaBola[order(somaBola$SomaTotal,decreasing = TRUE),]
-
+#Porcentagem de quanto saiu cada bola
 somaBola$Porcentagem <- (somaBola$SomaTotal/length(bolaTotal))*100
+somaBola
 
-porcentagem
 
-porcentagem$ordem <- c(1:60)
+
+#Dados ordenados de maneira decrescente (das bolas que mais sairam para as que menos sairam)
+somaBolaOrdenado <- somaBola[order(somaBola$SomaTotal,decreasing = TRUE),]
+somaBolaOrdenado
+
+
+#Adicionando uma coluna para ajudar a ordenar de 1 a 60 (das que mais saem para as que menos saem)
+somaBolaOrdenado$ordem <- c(1:60)
+somaBolaOrdenado
+
 
 plot(somaBola$Bola,somaBola$SomaTotal)
+hist(bolaTotal,breaks = 15)
+
+#Seguindo a teoria de probabilidades, numa situação ideal cada bola tem chance 1/60 de ser sorteada.
+#Ou seja, 1,666 % de ser sorteada se muitos sorteios forem realizados.
+#Considerando as porcentagens do dataframe, entendemos que as bolas que ainda não atingiram essa porcentagem
+#São as que tem mais tendência de serem sorteadas, para que o equilíbrio seja atingido matematicamente falando.
+#No caso desses dados são as bolas de número de ordem acima de 32
+#São calculadas pelo comando abaixo
+
+numerosdasorte <- somaBolaOrdenado$Bola[somaBolaOrdenado$ordem>=32]
+numerosdasorte
+
+#Exemplos de volantes que podem ser extraídos desses números
+for (i in 1:100){
+print(sample(numerosdasorte,6))
+  }
+
+
+
+
+
